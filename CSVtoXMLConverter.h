15be87c36c9204704f58239a327f6ce8bf1b2df9
Elavarasan <66453294+elavarasan3042000@users.noreply.github.com>
2024-06-13 16:37:39 +0530

    Update CSVtoXMLConverter.h

--- CSVtoXMLConverter.h
+++ CSVtoXMLConverter.h
@@ -10,6 +10,7 @@ void ConvertFromCSVToXML(FILE* stream) {
     TradeRecords Records[1024];
     int lineCount = 0;
     int objectCount = 0;
+    int LotSize = 10;
 
     while (fgets(line, sizeof(line), stream)) {
         char* fields[3];
@@ -33,16 +34,18 @@ void ConvertFromCSVToXML(FILE* stream) {
         int trade_amount;
         if (!TryConverttoInt(fields[1], &trade_amount)) {
             fprintf(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, fields[1]);
+            continue;
         }
 
         double trade_price;
         if (!TryConverttoDouble(fields[2], &trade_price)) {
             fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, fields[2]);
+            continue;
         }
 
         strncpy(Records[objectCount].SourceCurrency, fields[0], 3);
-        strncpy(Records[objectCount].DestibationCurrency, fields[0] + 3, 3);
-        Records[objectCount].Lots = trade_amount / LotSize;
+        strncpy(Records[objectCount].DestinationCurrency, fields[0] + 3, 3);
+        Records[objectCount].Lots = (int)(trade_amount / LotSize);
         Records[objectCount].Price = trade_price;
         objectCount++;
         lineCount++;
@@ -50,12 +53,12 @@ void ConvertFromCSVToXML(FILE* stream) {
 
     FILE* outFile = fopen("output.xml", "w");
     fprintf(outFile, "<TradeRecords>\n");
-    for (int i = 0; i < objectCount; i++) {
+    for (int object_index = 0; object_index < objectCount; object_index++) {
         fprintf(outFile, "\t<TradeRecord>\n");
-        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", Records[i].SourceCurrency);
-        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", Records[i].DestibationCurrency);
-        fprintf(outFile, "\t\t<Lots>%d</Lots>\n", Records[i].Lots);
-        fprintf(outFile, "\t\t<Price>%f</Price>\n", Records[i].Price);
+        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", Records[object_index].SourceCurrency);
+        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", Records[object_index].DestinationCurrency);
+        fprintf(outFile, "\t\t<Lots>%d</Lots>\n", Records[object_index].Lots);
+        fprintf(outFile, "\t\t<Price>%f</Price>\n", Records[object_index].Price);
         fprintf(outFile, "\t</TradeRecord>\n");
     }
     fprintf(outFile, "</TradeRecords>");
