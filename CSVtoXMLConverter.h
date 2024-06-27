e1d6a527711deaec1b0c41ffe880389d1b0840ab
Elavarasan <66453294+elavarasan3042000@users.noreply.github.com>
2024-06-27 13:01:44 +0530

    Update CSVtoXMLConverter.h

--- CSVtoXMLConverter.h
+++ CSVtoXMLConverter.h
@@ -210,7 +210,7 @@ void FreeLines(char** lines, int numLines) {
     free(lines);
 }
  
-void ConvertFromCsvtoXML(FILE* stream) {
+void ConvertFromCSCToXML(FILE* stream) {
     int numLines;
     char** lines = ReadTradeDataFromCsv(stream, &numLines);
     if (!lines) {
